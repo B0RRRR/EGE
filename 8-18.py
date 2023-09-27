e4687a2f4798e44f8d6764d49629653a14912362
Alex <37710886+BorussiaTop4ik@users.noreply.github.com>
2023-09-27 20:22:04 +0300

    Add files via upload

---
+++ 8-18.py
@@ -0,0 +1,17 @@
+from itertools import product, permutations
+sogl = glas = 0
+
+for i in range(2, 10):
+    k = 0
+    word = product('еия', repeat = i)
+    for j in word:
+        if j.count('е') <= 2 and j.count('и') <= 2 and j.count('я') <= 2:
+            k += 1
+    if i != 2:
+        glas += k
+    sogl += (3*k)
+
+print(sogl+glas)
+
+
+
