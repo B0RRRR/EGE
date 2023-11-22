a89bf1869b8c95672602f2920ea9bcae32a4d4c2
Alex <37710886+BorussiaTop4ik@users.noreply.github.com>
2023-11-22 18:58:21 +0300

    Add files via upload

---
+++ 16-19.py
@@ -0,0 +1,10 @@
+import sys
+sys.setrecursionlimit(10000)
+def f(n):
+    if n < 4 or n % 2 != 0:
+        return 1
+    if n > 3 and n % 2 == 0:
+        return f(n-1) + f(n-2) + f(n-3)
+
+
+print(f(2008)-f(2006))
