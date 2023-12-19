ce240b84f079eb7f766bf0ed4cd0af859f43ba5f
Alex <37710886+BorussiaTop4ik@users.noreply.github.com>
2023-12-19 21:21:37 +0300

    Add files via upload

---
+++ 13-11.py
@@ -0,0 +1,11 @@
+from ipaddress import *
+
+net = ip_network('192.168.32.160/255.255.255.240', 0)
+k = 0
+for ip in net:
+    ip_bin = bin(int(ip))[2:]
+    if ip_bin.count('1') % 2 == 0:
+        k += 1
+print(k)
+
+
